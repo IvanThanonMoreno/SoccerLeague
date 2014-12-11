e7f3e7c5a8dccebea866ca7778ab5900176dc460
IvanThanonMoreno <ivanmoreno199518@hotmail.com>
2014-12-11 21:04:20 +0000

    Create Points.java

---
+++ Points.java
@@ -0,0 +1,15 @@
+public class Points {
+    private int points;
+    
+    public Points () {}
+    
+    
+    public int getPoints () {
+        return this.points;
+    }
+    
+    
+    public void addPoints (int points) {
+        this.points += points;
+    }
+}
